e755759104dbe34c8a80011b27a087173274ed3f
Dominik Grzegolec <grzegolecdominik@gmail.com>
2022-12-09 15:44:21 +0100

    added working register form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,get_object_or_404,get_list_or_404
-
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Post,Comment
 
@@ -17,3 +17,14 @@ def detail(request, post_id):
     except Comment.DoesNotExist:
         return render(request, 'detail.html', {'post': post})
     return render(request, 'detail.html', {'post': post,'comment_list':comment_list})
+
+def register(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:index')
+    else:
+        form = UserCreationForm()
+    return render(request,'register.html',{'form':form})
