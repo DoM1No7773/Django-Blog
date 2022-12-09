82e00229a217d273c3f7be84ac5b8a74ab74ac61
Dominik Grzegolec <grzegolecdominik@gmail.com>
2022-12-09 13:44:12 +0100

    added detail feature and added model 'Comment'

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.content
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+
 
 from .models import Post
 
 # Create your views here.
 def index(request):
     post_list = Post.objects.order_by('created_at')[:5]
-    output = ', '.join([q.title for q in post_list])
-    return HttpResponse(output)
+    context = {'post_list': post_list}
+    return render(request, 'index.html', context)
 
 def detail(request, post_id):
-    return HttpResponse("post_id : %s." % post_id)
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'detail.html', {'post': post})
