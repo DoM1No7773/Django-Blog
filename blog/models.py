8f8a6f18505906a281de1acc8d972cede9b72a19
Dominik Grzegolec <grzegolecdominik@gmail.com>
2022-12-09 13:02:13 +0100

    displaying data on main page

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
     details = models.TextField()
     created_at = models.DateTimeField('date published')
-    #TODO: created_by ,but now i dont know how to do that 
+    created_by = models.ForeignKey(User, default=None, on_delete=models.CASCADE) 
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    post_list = Post.objects.order_by('created_at')[:5]
+    output = ', '.join([q.title for q in post_list])
+    return HttpResponse(output)
+
+def detail(request, post_id):
+    return HttpResponse("post_id : %s." % post_id)
