060c672805c164566f7e32037103774d8c66ce22
Dominik Grzegolec <grzegolecdominik@gmail.com>
2022-12-09 12:33:12 +0100

    created model 'Post' and superuser 'Admin'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    details = models.TextField()
+    created_at = models.DateTimeField('date published')
+    #TODO: created_by ,but now i dont know how to do that 
+
+    def __str__(self):
+        return self.title
